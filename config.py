145cd98b72e7cc8ef4355a6494e1a3fe75841f7b
Uzcoding <sardormaxmudov18@gmail.com>
2020-04-09 21:03:51 +0500

    initial commit

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-api_id = 0
-api_hash = ''
+api_id = 1350638
+api_hash = '798cde1c654a61c7cfbaf3063a31f932'
 session_name = 'session'
--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
-from PIL import Image, ImageFont, ImageDraw
+from pil import Image, ImageFont, ImageDraw
 from datetime import datetime
 from pytz import timezone
 
 
 def get_current_time():
-    return datetime.now(timezone('Europe/Moscow')).strftime('%H:%M')
+    return datetime.now(timezone('Asia/Tashkent')).strftime('%H:%M')
 
 
 def generate_image(text):
@@ -16,4 +16,4 @@ def generate_image(text):
     wt, ht = draw.textsize(text, font=font)
     draw.text(((W - wt) / 2, (H - ht) / 2 ), text, font=font, fill='#157381')
 
-    image.save('time_image.jpg')
+    image.save('time_image.jpg') 
